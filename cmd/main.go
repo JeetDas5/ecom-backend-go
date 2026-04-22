6a4a954bc4ef70f7c9d4f8a8839bf61a2adae0c0
JeetDas5 <jeet15083011@gmail.com>
2026-04-22 21:08:31 +0530

    database setup

--- cmd/main.go
+++ cmd/main.go
@@ -1,23 +1,44 @@
 package main
 
 import (
+	"context"
+	"github/JeetDas5/ecom-app/internal/env"
 	"log"
 	"log/slog"
 	"os"
+
+	"github.com/jackc/pgx/v5"
 )
 
 func main() {
+	ctx := context.Background()
 	cfg := config{
 		addr: ":8080",
-		db:   dbConfig{},
-	}
-
-	api := application{
-		config: cfg,
+		db: dbConfig{
+			dsn: env.GetString(
+				"GOOSE_DBSTRING",
+				"host=localhost port=5434 user=postgres password=postgres dbname=ecom sslmode=disable",
+			),
+		},
 	}
 
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
 	slog.SetDefault(logger)
+
+	//database
+	conn, err := pgx.Connect(ctx, cfg.db.dsn)
+	//print db string
+	logger.Info("database connection string", "dsn", cfg.db.dsn)
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close(ctx)
+
+	logger.Info("connected to database", "dsn", cfg.db.dsn)
+
+	api := application{
+		config: cfg,
+	}
 
 	slog.Info("starting server", "addr", api.config.addr)
 
